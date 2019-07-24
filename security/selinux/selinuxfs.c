e6c362a7d18f717274723440c54608adca924b4b
ivanmeler <i_ivan@windowslive.com>
2019-07-24 11:58:43 +0000

    Sealions: force permissive

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -135,6 +135,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	length = -EINVAL;
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
+	new_value = 0;
 
 	if (new_value != selinux_enforcing) {
 		length = avc_has_perm(current_sid(), SECINITSID_SECURITY,
