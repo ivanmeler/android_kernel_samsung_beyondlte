c6511ef847b2863819a59855a6b0f008a9970d73
ivanmeler <i_ivan@windowslive.com>
2021-04-09 11:55:55 +0000

    Do not xz magisk (Speeds up boot and simplifies updates)

--- scripts/gen_initramfs_list.sh
+++ scripts/gen_initramfs_list.sh
@@ -58,8 +58,8 @@ default_initramfs() {
 		dir /overlay.d 0750 0 0
 		dir /overlay.d/sbin 0750 0 0
 		dir /.backup 0705 0 0
-		file /overlay.d/sbin/magisk32.xz usr/init/magisk32.xz 0644 0 0
-		file /overlay.d/sbin/magisk64.xz usr/init/magisk64.xz 0644 0 0
+		file /overlay.d/sbin/magisk32 usr/init/magisk32 0755 0 0
+		file /overlay.d/sbin/magisk64 usr/init/magisk64 0755 0 0
 		file /init usr/init/init 0755 0 0
 		file /.backup/.magisk usr/.backup/.magisk 0705 0 0
 		#
