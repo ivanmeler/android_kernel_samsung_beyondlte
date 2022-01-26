67a5bbd15ff3171308ac1d2982ccac0d8288544f
ivanmeler <i_ivan@windowslive.com>
2022-01-26 12:55:41 +0000

    Add beta and local file support to magisk updater

--- update_magisk.sh
+++ update_magisk.sh
@@ -2,30 +2,53 @@
 
 export BRANCH=$1
 color_red='\033[0;31m'
+color_green='\033[0;32m'
 color_reset='\033[0m'
+should_proceed=0
 
 case $BRANCH in
 stable)
-	wget `curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json | jq -r '.magisk.link'` -O Magisk.zip
-	magisk_ver=`curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json | jq -r '.magisk.version'`
+	is_remote_download=1
         should_proceed=1
+	revision=stable
 	;;
+beta)
+	is_remote_download=1
+        should_proceed=1
+	revision=beta
+        ;;
 canary)
-	wget `curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/canary.json | jq -r '.magisk.link'` -O Magisk.zip
-	magisk_ver=`curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/canary.json | jq -r '.magisk.version'`
+	is_remote_download=1
         should_proceed=1
+	revision=canary
 	;;
+local)
+        is_remote_download=0
+        magisk_ver="local fork"
+	if test -f "Magisk.zip"; then
+        	should_proceed=1
+	fi
+        ;;
 *)
         echo -e "$color_red"
-	echo -e "Run script with branch you want to updaye to"
+	echo -e "Run script with branch you want to update to"
 	echo -e "e.g."
-	echo -e "update_magisk.sh canary or update_magisk.sh stable"
+	echo -e "update_magisk.sh stable/beta/canary/local"
+	echo -e "$color_green"
+	echo -e "When using local place Magisk.zip in root dir of kernel src."
         echo -e "$color_reset"
 	should_proceed=0
 	;;
 esac
 
+
+
 if [ $should_proceed -eq "1" ]; then
+
+if [ $is_remote_download -eq "1" ]; then
+    wget `curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/$revision.json | jq -r '.magisk.link'` -O Magisk.zip
+    magisk_ver=`curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/$revision.json | jq -r '.magisk.version'`
+fi
 
 # Delete old Magisk version
 rm -rf usr/init/magisk32.xz
